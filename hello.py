c83fc771b0987f39b54d6066d38fc6eb6a777e15
kite <zyj987324339@163.com>
2020-10-29 18:20:20 +0800

    add python file to 'hello world'

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#########################################################################
+# -*- coding: utf-8 -*-
+# File Name: hello.py
+# Author :kite
+# QQ : 987324339
+# Email:987324339@qq.com
+# Created Time: Thu 29 Oct 2020 06:19:03 PM CST
+#########################################################################
+
+def say():
+    print("hello world\n")
