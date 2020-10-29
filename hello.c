01b553d09a79f4918ce559801b5744408344064c
kite <zyj987324339@163.com>
2020-10-29 11:37:44 +0800

    add hello world for 'hello.c' file

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+  * File Name: hello.c
+  * Author : kite
+  * QQ : 987324339
+  * Created Time: Thu 29 Oct 2020 11:36:55 AM CST
+  * Email:987324339@qq.com
+**/
+#include <stdio.h>
+
+int main()
+{
+  printf("hello world\n");
+
+  return 0;
+}
