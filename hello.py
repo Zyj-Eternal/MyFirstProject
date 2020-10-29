110f398a15932a6f302c3c4e0edb5df01beaf46f
kite <zyj987324339@163.com>
2020-10-29 18:29:10 +0800

    add getResponse method

--- hello.py
+++ hello.py
@@ -7,6 +7,17 @@
 # Email:987324339@qq.com
 # Created Time: Thu 29 Oct 2020 06:19:03 PM CST
 #########################################################################
+import re
+import requests
+
 
 def say():
     print("hello world\n")
+
+
+def getResponse():
+    response = requests.get('https://www.baidu.com').text
+    print(response)
+
+
+getResponse()
