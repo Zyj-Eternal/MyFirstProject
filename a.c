52a88bec8f1a7785b9f3a2658b1a3c637cd18ebe
kite <zyj987324339@163.com>
2020-10-31 22:58:58 +0800

    add 'hello world' to this program

---
+++ a.c
@@ -0,0 +1,15 @@
+/**
+  * File Name: a.c
+  * Author : kite
+  * QQ : 987324339
+  * Created Time: Sat 31 Oct 2020 10:58:07 PM CST
+  * Email:987324339@qq.com
+**/
+#include <stdio.h>
+
+int main()
+{
+  printf("hello world\n");
+
+  return 0;
+}
